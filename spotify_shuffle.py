c7d95773a2626786de7c685a706cb044c02e213e
yzimroni <yanay03@gmail.com>
2019-01-19 11:40:40 +0200

    Shuffle library and add result to playlist

--- spotify_shuffle.py
+++ spotify_shuffle.py
@@ -1,11 +1,15 @@
 import sys
 import spotipy
 import spotipy.util as util
+import random
+import os
 
 scope = 'user-library-read playlist-read-private playlist-modify-private playlist-modify-public ' \
         'user-read-recently-played user-top-read'
 
 token = util.prompt_for_user_token("spotify_user", scope)
+
+PLAYLIST_SIZE = 100
 
 if not token:
     sys.exit()
@@ -26,4 +30,22 @@ def get_all_items(source, per_page):
     return {"items": items}
 
 
-library_songs = get_all_items(lambda o: sp.current_user_saved_tracks(limit=50, offset=o), 50)
+def run():
+    print("Running SpotifyShuffle")
+    user_id = sp.me()["id"]
+    playlist_id = os.getenv('SHUFFLE_PLAYLIST_ID')
+    print("Spotify user ID: %s" % user_id)
+    print("Spotify playlist ID: %s" % playlist_id)
+
+    print("Loading library")
+    library_songs = get_all_items(lambda o: sp.current_user_saved_tracks(limit=50, offset=o), 50)["items"]
+    print("Library loaded, %s songs found" % len(library_songs))
+    random.shuffle(library_songs)
+    track_ids = [s["track"]["id"] for s in library_songs[:PLAYLIST_SIZE]]
+
+    sp.user_playlist_replace_tracks(user_id, playlist_id, track_ids)
+    print("Updated playlist id %s with %s randomized songs" % (playlist_id, len(track_ids)))
+
+
+if __name__ == "__main__":
+    run()
