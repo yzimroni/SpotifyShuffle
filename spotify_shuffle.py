e8cc66f685b87cfae04e227e226829039a353555
yzimroni <yanay03@gmail.com>
2019-03-16 18:01:54 +0200

    Update playlist description

--- spotify_shuffle.py
+++ spotify_shuffle.py
@@ -3,6 +3,7 @@ import spotipy
 import spotipy.util as util
 import random
 import os
+import datetime
 
 scope = 'user-library-read playlist-read-private playlist-modify-private playlist-modify-public ' \
         'user-read-recently-played user-top-read'
@@ -44,6 +45,9 @@ def run():
     track_ids = [s["track"]["id"] for s in library_songs[:PLAYLIST_SIZE]]
 
     sp.user_playlist_replace_tracks(user_id, playlist_id, track_ids)
+    sp.user_playlist_change_details(user_id, sp._get_id("playlist", playlist_id), description="""Automatically shuffled by SpotifyShuffle
+Last shuffle date: %s
+Playlist size: %s (%s)""" % (datetime.date.today().strftime("%H:%M:%S %d.%m.%Y"), len(track_ids), PLAYLIST_SIZE))
     print("Updated playlist id %s with %s randomized songs" % (playlist_id, len(track_ids)))
 
 
